459b24149583087fef3a07c6bc0178ce0a334ec9
HwangHarim <scopan01@naver.com>
2023-04-12 23:39:50 +0900

    :tada: init: test init

---
+++ src/main/java/com/test/study/SpringBootTestStudyApplication.java
@@ -0,0 +1,13 @@
+package com.test.study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootTestStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootTestStudyApplication.class, args);
+    }
+
+}
